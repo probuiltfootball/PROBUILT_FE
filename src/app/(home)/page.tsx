f6936e67208c2e75d84a3bdb1c71f8953a07da08
prakruti pareek <prakruti.kapadia@gmail.com>
2025-12-05 00:03:55 +0000

    footer updated

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import HubSection from "@/components/hub-section";
 import TeamSection from "@/components/team-section";
 import TestimonialsSection from "@/components/testimonials-section";
 import CtaSection from "@/components/cta-section";
-import { Footer } from "@/components/footer";
+import { Footer } from "@/components/footerv2";
 
 export default function Home() {
   return (
